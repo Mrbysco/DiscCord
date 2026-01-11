a33c43e9f3b593af7eb458e0a13eeb14d9bf89b0
Mrbysco <Mrbysco@users.noreply.github.com>
2026-01-11 16:58:14 +0100

    Fix play record payload

--- src/main/java/com/mrbysco/disccord/network/payload/PlayRecordPayload.java
+++ src/main/java/com/mrbysco/disccord/network/payload/PlayRecordPayload.java
@@ -3,7 +3,6 @@ package com.mrbysco.disccord.network.payload;
 import com.mrbysco.disccord.DiscCordMod;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.UUIDUtil;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
@@ -21,23 +20,14 @@ public record PlayRecordPayload(BlockPos pos, String url, UUID uuid, int entityI
 			payload -> payload.url,
 			UUIDUtil.STREAM_CODEC,
 			payload -> payload.uuid,
+			ByteBufCodecs.INT,
+			payload -> payload.entityId,
 			PlayRecordPayload::new
 	);
 	public static final Type<PlayRecordPayload> ID = new Type<>(DiscCordMod.modLoc("play_record"));
 
 	public PlayRecordPayload(BlockPos pos, String url) {
 		this(pos, url, Util.NIL_UUID, -1);
-	}
-
-	public PlayRecordPayload(FriendlyByteBuf buf) {
-		this(buf.readBlockPos(), buf.readUtf(), buf.readUUID(), buf.readInt());
-	}
-
-	public void write(FriendlyByteBuf buf) {
-		buf.writeBlockPos(pos);
-		buf.writeUtf(url);
-		buf.writeUUID(uuid);
-		buf.writeInt(entityId);
 	}
 
 	@Override
